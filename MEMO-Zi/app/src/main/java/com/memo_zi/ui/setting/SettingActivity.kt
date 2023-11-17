2ce2645a8fe7582f2f7abeab1a1dbc12887b479a
Do-Re-Min-Woo <kimy5878@naver.com>
2023-11-17 19:20:26 +0900

    Add - 설정 화면 추가

---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/ui/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.memo_zi.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.memo_zi.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
