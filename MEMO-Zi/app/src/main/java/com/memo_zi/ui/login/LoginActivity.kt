619280e92942bb62ea885d1a476e0c3e03dfa141
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-11-20 16:21:11 +0900

    Feat : login 텍스트 로고 그라데이션

---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/ui/login/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.memo_zi.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.text.set
+import androidx.core.text.toSpannable
+import com.memo_zi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val txtHello: TextView = findViewById(R.id.logoText)
+        val text = TITLE_TEXT
+        val startColorInt = ContextCompat.getColor(this, R.color.bg_start)
+        val endColorInt = ContextCompat.getColor(this, R.color.bg_end)
+        val spannable = text.toSpannable()
+        spannable[0..text.length] = LinearGradientSpan(text, text, startColorInt, endColorInt)
+        txtHello.text = spannable
+    }
+
+    companion object {
+        const val TITLE_TEXT = "MEMO : Zi"
+    }
+}
+
