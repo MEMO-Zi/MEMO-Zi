9391ec74ae5769521b4417f14eb6e8c4c36f4f70
cacaocoffee <twinaudtjr@naver.com>
2024-01-19 12:28:10 +0900

    TODO :  카테고리 화면으로 변환시 viewpager에서 선택된  아이템 넘기기

--- MEMO-Zi/app/src/main/java/com/memo_zi/ui/memo/MemoCategoryFragment.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/ui/memo/MemoCategoryFragment.kt
@@ -26,7 +26,7 @@ class MemoCategoryFragment : BindingFragment<FragmentMemoCategoryEditBinding>( )
 
     private fun initAdapter() {
         categoryAdapter = MemoCategoryAdapter(requireContext())
-
+//todo 카테고리 화면으로 변환시 viewpager에서 선택된  아이템 넘기기
     }
 
     private fun setCategory() {
