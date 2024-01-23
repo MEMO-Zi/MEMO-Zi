b07e31ea72d1ddadd586975b4aefa6fc7c9a212d
MIN WOO KIM <kimy5878@naver.com>
2024-01-23 18:07:18 +0900

    [chore] #17 파일 이름 변경

--- MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/diary/adapter/DiaryListAllViewHolder.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/diary/adapter/DiaryListAllViewHolder.kt
@@ -2,10 +2,11 @@ package com.memo_zi.presentation.ui.diary.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.memo_zi.databinding.ItemDiaryFeedBinding
 import com.memo_zi.presentation.model.DiaryItem
-import com.memo_zi.databinding.ItemDiaryListAllBinding
 
-class DiaryListAllViewHolder(private val binding: ItemDiaryListAllBinding) : RecyclerView.ViewHolder(binding.root) {
+class DiaryListAllViewHolder(private val binding: ItemDiaryFeedBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(diaryData: DiaryItem.AllItem) {
         binding.run {
