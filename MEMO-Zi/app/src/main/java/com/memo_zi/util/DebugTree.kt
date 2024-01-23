b2678cc6dc0d20d6fc7138794e5ce3a1e0ad57d1
MIN WOO KIM <kimy5878@naver.com>
2024-01-23 21:38:23 +0900

    [fix] #17 Hilt 문제 해결

---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/util/DebugTree.kt
@@ -0,0 +1,8 @@
+package com.memo_zi.util
+
+import timber.log.Timber
+
+class DebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement) =
+        "${element.fileName}:${element.lineNumber}#${element.methodName}"
+}
