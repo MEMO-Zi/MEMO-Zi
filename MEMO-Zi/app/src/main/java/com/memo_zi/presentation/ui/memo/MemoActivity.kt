27ecbc7d2e2cdd9744228786d6626c501b3c5c92
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-01-30 21:33:28 +0900

    [del] #30 사용하지 않는 drawable 제거

--- MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoActivity.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoActivity.kt
@@ -11,8 +11,6 @@ import androidx.viewpager2.widget.ViewPager2
 import com.hadi.viewpager2carousel.MemoAdapter
 import com.memo_zi.R
 import com.memo_zi.databinding.ActivityMemoBinding
-import com.memo_zi.presentation.ui.diary.DiaryActivity
-import com.memo_zi.presentation.ui.setting.SettingActivity
 import com.memo_zi.util.binding.BindingActivity
 import kotlin.math.absoluteValue
 
