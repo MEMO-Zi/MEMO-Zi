5e99927b6fab729ce25a22495b89248beb6be693
김명석 <twinaudtjr@naver.com>
2024-02-10 05:01:36 +0900

    [feat] #35 온보딩 페이지 로직 연결 첫번째 fragment

---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,24 @@
+package com.memo_zi.presentation.ui.onboarding;
+
+import android.os.Bundle
+import com.memo_zi.R
+import com.memo_zi.databinding.ActivityOnboardingBinding
+import com.memo_zi.util.binding.BindingActivity
+
+class OnboardingActivity :
+    BindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+    private lateinit var onboardingAdapter: OnboardingAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        onboardingAdapter = OnboardingAdapter(this)
+        binding.run {
+            vpOnboarding.adapter = onboardingAdapter
+            indicatorOnboarding.setViewPager(vpOnboarding)
+        }
+    }
+
+}
---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/onboarding/OnboardingAdapter.kt
@@ -0,0 +1,20 @@
+package com.memo_zi.presentation.ui.onboarding
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+class OnboardingAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    private val fragments =
+        listOf<Fragment>(OnboardingGreetingFragment(), OnboardingGreetingFragment())
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
