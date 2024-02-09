bab33138fd0f91169ce9bbd953d5bc58296bc2de
김명석 <twinaudtjr@naver.com>
2024-02-09 15:59:22 +0900

    [feat] #35 온보딩 메인페이지

---
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/onboarding/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package com.memo_zi.presentation.ui.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.memo_zi.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
