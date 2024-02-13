bd62adaa0d299be106e1a4129e8885e7c21e22f0
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-02-13 15:55:21 +0900

    [chore] #40 매개변수 이름 추가

--- MEMO-Zi/app/src/main/java/com/memo_zi/util/component/TextGradation.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/util/component/TextGradation.kt
@@ -12,9 +12,10 @@ class TextGradation(
     init {
         val spannable = textView.text.toSpannable()
         spannable[START_INDEX..textView.text.toString().length] = LinearGradientSpan(
-            context,
-            textView.text.toString(),
-            textView.text.toString().substring(START_INDEX, textView.text.toString().length)
+            context = context,
+            containingText = textView.text.toString(),
+            textToStyle = textView.text.toString()
+                .substring(START_INDEX, textView.text.toString().length)
         )
         textView.text = spannable
     }
