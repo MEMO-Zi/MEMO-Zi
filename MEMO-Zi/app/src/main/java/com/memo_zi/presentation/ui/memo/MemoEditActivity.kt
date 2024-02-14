cc02a90dd3a49a47842de0b69637edd3859276df
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-02-14 17:31:15 +0900

    [feat] #39 메모 edit 화면에 텍스트에 따른 버튼 활성화 등록

--- MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoEditActivity.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoEditActivity.kt
@@ -2,6 +2,7 @@ package com.memo_zi.presentation.ui.memo
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import com.memo_zi.R
 import com.memo_zi.databinding.ActivityMemoEditBinding
 import com.memo_zi.util.binding.BindingActivity
@@ -11,7 +12,12 @@ class MemoEditActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initLayout()
+    }
+
+    private fun initLayout() {
         setupButton()
+        setButtonEnable()
     }
 
     private fun setupButton() {
@@ -24,6 +30,21 @@ class MemoEditActivity :
 //            memoBold.setOnClickListener {
 //todo 메모 볼드체 등 기타 버튼에 대한 설정 필요
 //            }
+
+            etMemoEditContents.addTextChangedListener{
+                setButtonEnable()
+            }
+            etMemoEditTitle.addTextChangedListener {
+                setButtonEnable()
+            }
+        }
+
+    }
+
+    private fun setButtonEnable() {
+        binding.run {
+            btnMemoEditEnroll.isEnabled =
+                etMemoEditTitle.text.isNotEmpty() && etMemoEditContents.text.isNotEmpty()
         }
     }
 }
