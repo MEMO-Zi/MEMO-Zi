9526d575b94a8c58fd20a093feb578bfa1927cde
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-02-20 20:21:49 +0900

    [feat] #39 메모 생성시 카테고리 바텀 시트 나오게 설정

--- MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoEditActivity.kt
+++ MEMO-Zi/app/src/main/java/com/memo_zi/presentation/ui/memo/MemoEditActivity.kt
@@ -3,12 +3,14 @@ package com.memo_zi.presentation.ui.memo
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.memo_zi.R
 import com.memo_zi.databinding.ActivityMemoEditBinding
 import com.memo_zi.util.binding.BindingActivity
 
-class MemoEditActivity :
-    BindingActivity<ActivityMemoEditBinding>(R.layout.activity_memo_edit) {
+
+class MemoEditActivity : BindingActivity<ActivityMemoEditBinding>(R.layout.activity_memo_edit) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,27 +30,45 @@ class MemoEditActivity :
                 }
             }
 
-            layoutMemoEditCategory.setOnClickListener{
-                //todo 카테고리
+            layoutMemoEditCategory.setOnClickListener {
+                setBottomSheet()
             }
 //            memoBold.setOnClickListener {
 //todo 메모 볼드체 등 기타 버튼에 대한 설정 필요
 //            }
 
-            etMemoEditContents.addTextChangedListener{
+            etMemoEditContents.addTextChangedListener {
                 setButtonEnable()
             }
             etMemoEditTitle.addTextChangedListener {
                 setButtonEnable()
             }
         }
-
     }
+
+    private fun setBottomSheet() {
+        val bottomSheetView =
+            layoutInflater.inflate(R.layout.bottom_sheet_memo_category_select, null)
+        val bottomSheetDialog = BottomSheetDialog(this@MemoEditActivity)
+        val offsetFromTop = MARGIN_BOTTOM_SHEET
+        (bottomSheetDialog as? BottomSheetDialog)?.behavior?.apply {
+            isFitToContents = false
+            expandedOffset = offsetFromTop
+            state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
+    }
+
 
     private fun setButtonEnable() {
         binding.run {
             btnMemoEditEnroll.isEnabled =
                 etMemoEditTitle.text.isNotEmpty() && etMemoEditContents.text.isNotEmpty()
         }
+    }
+
+    companion object {
+        const val MARGIN_BOTTOM_SHEET = 94
     }
 }
